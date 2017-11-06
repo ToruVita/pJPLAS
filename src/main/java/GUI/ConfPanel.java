9b3794316de36c2eefebed6120d0ee0b2a3010cf
Toru.vita <toru.vita@gmail.com>
2017-11-07 08:35:41 +0900

    ConfTable ..2

--- src/main/java/GUI/ConfPanel.java
+++ src/main/java/GUI/ConfPanel.java
@@ -1,28 +1,42 @@
 package GUI;
 
 import java.awt.Component;
-import java.util.EventObject;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.HashMap;
 
+import javax.swing.AbstractCellEditor;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JTable;
-import javax.swing.event.CellEditorListener;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.TableCellEditor;
 import javax.swing.table.TableModel;
 
-public class ConfPanel extends JTable{
+public class ConfPanel extends JTable {
 	ConfTableModel model = new ConfTableModel();;
-	ConfPanel(){
+
+	ConfPanel() {
 		super();
 		setModel(model);
 		ConfTableCellEditor anEditor = new ConfTableCellEditor();
 		setCellEditor(anEditor);
 	}
-	class ConfTableCellEditor implements TableCellEditor{
+
+	class ConfTableCellEditor implements TableCellEditor, ActionListener {
+		JFileChooser filec = new JFileChooser();
+		JButton button = new JButton();
+
+		ConfTableCellEditor() {
+			button.addActionListener(this);
+		}
 
 		public Object getCellEditorValue() {
-			// TODO Auto-generated method stub
-			return null;
+			if (filec.getSelectedFile() == null) return null;
+			Icon icon = new ImageIcon(filec.getSelectedFile().getPath());
+			return icon;
 		}
 
 		public boolean isCellEditable(EventObject anEvent) {
@@ -42,94 +56,108 @@ public class ConfPanel extends JTable{
 
 		public void cancelCellEditing() {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		public void addCellEditorListener(CellEditorListener l) {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		public void removeCellEditorListener(CellEditorListener l) {
 			// TODO Auto-generated method stub
-			
-		}
 
+		}
 		public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row,
 				int column) {
-			// TODO Auto-generated method stub
-			return null;
+			if (value == null){
+				button.setText("value is Null");
+			}else{
+				button.setText(value.toString());
+			}
+
+			if (value instanceof Icon){
+				button.setIcon((Icon)value);
+			}else{
+				button.setIcon(null);
+			}
+			return button;
 		}
-		
-	}
-	public void setConf(String key, Object o){
-		model.setPair(key, o);
-	}
-	class ConfTableModel implements TableModel{
-		class ConfPairObject{
-			Component component;
-			Object object;
-			public ConfPairObject(Component component, Object object) {
-				super();
-				this.component = component;
-				this.object = object;
+
+		public void actionPerformed(ActionEvent e) {
+			if (JFileChooser.APPROVE_OPTION == filec.showOpenDialog(button)) {
+				stopCellEditing();
 			}
 		}
-		private HashMap <String, ConfPairObject> conf = new HashMap <String, ConfPairObject>();
-		void setPair(String key, Object o){
-			conf.put(key, new ConfPairObject(null, o));
+
+	}
+
+	public void setConf(String key, Object o) {
+		model.set(key, o);
+	}
+
+	class ConfTableModel implements TableModel {
+		private HashMap<String, Object> conf = new HashMap<String, Object>();
+
+		void set(String key, Object o) {
+			conf.put(key, o);
 		}
+
 		public int getRowCount() {
 			return conf.size();
 		}
+
 		public int getColumnCount() {
 			return 2;
 		}
+
 		public String getColumnName(int columnIndex) {
-			if(columnIndex==0){
+			if (columnIndex == 0) {
 				return "KEY";
-			}else{
+			} else {
 				return "VALUE";
 			}
 		}
+
 		public Class<?> getColumnClass(int columnIndex) {
-			if(columnIndex==0){
+			if (columnIndex == 0) {
 				return "KEY".getClass();
-			}else{
+			} else {
 				return Object.class;
 			}
 		}
 
 		public boolean isCellEditable(int rowIndex, int columnIndex) {
-			if(columnIndex==0){
+			if (columnIndex == 0) {
 				return false;
-			}else{
+			} else {
 				return true;
 			}
 		}
 
 		public Object getValueAt(int rowIndex, int columnIndex) {
-			if(columnIndex==0){
-				return conf.keySet().toArray()[rowIndex].toString();
-			}else{
-				return conf.get(conf.keySet().toArray()[rowIndex]).toString();
+			String ky = conf.keySet().toArray()[rowIndex].toString();
+			if (columnIndex == 0) {
+				return ky;
+			} else {
+				return conf.get(ky).getClass().toString() + conf.get(ky).toString();
 			}
 		}
 
 		public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-			// TODO Auto-generated method stub
-			
+			String ky = conf.keySet().toArray()[rowIndex].toString();
+			conf.put(ky, aValue);
 		}
 
 		public void addTableModelListener(TableModelListener l) {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		public void removeTableModelListener(TableModelListener l) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
 	}
 }
--- src/main/java/GUI/TestFrame.java
+++ src/main/java/GUI/TestFrame.java
@@ -1,5 +1,6 @@
 package GUI;
 
+import java.io.File;
 import java.util.Date;
 
 public class TestFrame extends Frame {
@@ -11,6 +12,12 @@ public class TestFrame extends Frame {
 		setDefaultMenu();
 		ConfPanel cp = new ConfPanel();
 		cp.setConf("Oro", new Date());
+		cp.setConf("Oro2", "H265");
+		cp.setConf("OroF", new File("/"));
+		cp.setConf("Oro3", new Integer(23));
 		setPlate(cp);
+		Object o = new File("c:\\");
+		System.out.println(o.toString());
+		System.out.println(o.getClass());
 	}
 }
